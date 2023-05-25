7632d5360108fab68fab5f0ea303f6998d69c96b
yunusemrebagca <a7yunusemre34@gmail.com>
2023-05-26 01:42:52 +0300

    feat: table of contents added

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 // app/posts/[slug]/page.tsx
 import { allPosts } from "contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { notFound } from "next/navigation";
 import type { MDXComponents } from "mdx/types";
 import s from "./page.module.scss";
+import parameterize from "parameterize-js";
 
 import "highlight.js/scss/arta.scss";
 
 import Link from "next/link";
 import { Hero } from "@/components/Hero";
+import clsx from "clsx";
+import { useState } from "react";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -25,7 +30,11 @@ const mdxComponents: MDXComponents = {
   ),
   // Add a custom component.
   p: ({ children }) => <p className={s.paragraph}>{children}</p>,
-  h2: ({ children }) => <h2 className={s.h2}>{children}</h2>,
+  h2: ({ children }) => (
+    <h2 id={parameterize(children)} className={s.h2}>
+      {children}
+    </h2>
+  ),
   img: (props) => <img className={s.img} {...props}></img>,
   li: (props) => (
     <li className={s.li} {...props}>
@@ -72,12 +81,57 @@ export default function Page({ params }: { params: { slug: string } }) {
   // 404 if the post does not exist.
   if (!post) notFound();
 
+  function getHeadings(source) {
+    // Get each line individually, and filter out anything that
+    // isn't a heading.
+    const headingLines = source.split("\n").filter((line) => {
+      return line.match(/^###*\s/);
+    });
+
+    // Transform the string '## Some text' into an object
+    // with the shape '{ text: 'Some text', level: 2 }'
+    return headingLines.map((raw) => {
+      const text = raw.replace(/^###*\s/, "");
+      // I only care about h2 and h3.
+      // If I wanted more levels, I'd need to count the
+      // number of #s.
+      const level = raw.slice(0, 3) === "###" ? 3 : 2;
+
+      return { text, level };
+    });
+
+    return headingLines;
+  }
+
+  const headings = getHeadings(post.body.raw);
+  const headingLinks = headings.map((heading) => {
+    return parameterize(heading.text);
+  });
+
   // Parse the MDX file via the useMDXComponent hook.
   const MDXContent = useMDXComponent(post.body.code);
+
+  const [toggleToc, setToggleToc] = useState(false);
 
   return (
     <>
       <Hero title={post.title} postImage={post.imageUrl} />
+      <div className={clsx(s.toc__div, { [s.active]: toggleToc })}>
+        <ul className={s.toc}>
+          {headings.map((heading, idx) => {
+            return (
+              <li key={idx}>
+                <Link href={`/${params.slug}/#${parameterize(heading.text)}`}>
+                  {heading.text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className={s.button} onClick={() => setToggleToc(!toggleToc)}>
+        Table of Contents
+      </div>
       <div className={s.content}>
         <MDXContent components={mdxComponents} />
       </div>
